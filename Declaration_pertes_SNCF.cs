9201849a25ff28f30aa610a518abd3951b41d36e
ojammefr <43725847+ojammefr@users.noreply.github.com>
2020-07-14 19:39:01 +0200

    Archiver initiale du module LoadDataAPISNCF

---
+++ Declaration_pertes_SNCF.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace Test_newtonsoft
+{
+    public class Parameters
+    {
+        public string dataset { get; set; }
+        public string timezone { get; set; }
+        public int rows { get; set; }
+        public string format { get; set; }
+        public List<string> facet { get; set; }
+    }
+
+    public class Fields
+    {
+        public string gc_obo_nature_c { get; set; }
+        public string gc_obo_gare_origine_r_name { get; set; }
+        public DateTime date { get; set; }
+        public string gc_obo_nom_recordtype_sc_c { get; set; }
+        public string gc_obo_type_c { get; set; }
+        public string gc_obo_gare_origine_r_code_uic_c { get; set; }
+    }
+
+    public class Record
+    {
+        public string datasetid { get; set; }
+        public string recordid { get; set; }
+        public Fields fields { get; set; }
+        public DateTime record_timestamp { get; set; }
+    }
+
+    public class RecDB
+    {
+        public string datasetid { get; set; }
+        public string recordid { get; set; }
+        //public Fields fields { get; set; }
+        public string gc_obo_nature_c { get; set; }
+        public string gc_obo_gare_origine_r_name { get; set; }
+        public DateTime date { get; set; }
+        public string gc_obo_nom_recordtype_sc_c { get; set; }
+        public string gc_obo_type_c { get; set; }
+        public string gc_obo_gare_origine_r_code_uic_c { get; set; }
+        public DateTime record_timestamp { get; set; }
+    }
+
+    public class Facet
+    {
+        public int count { get; set; }
+        public string path { get; set; }
+        public string state { get; set; }
+        public string name { get; set; }
+    }
+
+    public class FacetGroup
+    {
+        public List<Facet> facets { get; set; }
+        public string name { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int nhits { get; set; }
+        public Parameters parameters { get; set; }
+        public List<Record> records { get; set; }
+        public List<FacetGroup> facet_groups { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+//using System.Net;
+using System.IO;
+using System.Net.Http;
+using Raven.Client.Documents;
+using Raven.Client.Documents.BulkInsert;
+
+namespace Test_newtonsoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var store = new DocumentStore
+            {
+                Urls = new[] { "http://localhost:8081" },
+                Database = "Objets_Trouves"
+            };
+            store.Initialize();
+
+            HttpClient client = new HttpClient();
+            String url = "https://data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-gares&rows=10000&facet=date&facet=gc_obo_gare_origine_r_name&facet=gc_obo_type_c";
+            int i = 0;
+
+            using (BulkInsertOperation bulkInsert = store.BulkInsert())
+            using (Stream s = client.GetStreamAsync(url).Result)
+            using (StreamReader sr = new StreamReader(s))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+
+                // read the json from a stream
+                RootObject p = serializer.Deserialize<RootObject>(reader);
+                List<Record> rec = p.records;
+                foreach (var repo in rec)
+                {
+                    var recdb = new RecDB
+                    {
+
+                        datasetid = repo.datasetid,
+                        recordid = repo.recordid,
+                        record_timestamp = repo.record_timestamp,
+                        gc_obo_gare_origine_r_code_uic_c = repo.fields.gc_obo_gare_origine_r_code_uic_c,
+                        gc_obo_nature_c = repo.fields.gc_obo_nature_c,
+                        gc_obo_gare_origine_r_name = repo.fields.gc_obo_gare_origine_r_name,
+                        date = repo.fields.date,
+                        gc_obo_nom_recordtype_sc_c = repo.fields.gc_obo_nom_recordtype_sc_c,
+                        gc_obo_type_c = repo.fields.gc_obo_type_c
+
+                    };
+                    bulkInsert.Store(recdb);
+                    i++;
+                    Console.WriteLine("i="+i);
+                }
+                Console.WriteLine("FIN");
+            }
+
+        }
+    }
+}
